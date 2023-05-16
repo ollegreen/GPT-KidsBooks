64288193bfa30abe2c7671c65691b8b0b8635b8e
ollegreen <olleg.green@gmail.com>
2023-05-16 22:38:28 +0100

    fix first v

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import requests
 import streamlit as st
-#from resources.api.api_key import the_api_key
+from resources.secrets.secret import the_secret_sauce
 
 st.write('Hello unknown storyteller!')
 
 API_URL = "https://api-inference.huggingface.co/models/gpt2"
-headers = {"Authorization": f"{api_key}"}
+headers = {"Authorization": f"{the_secret_sauce}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
---
+++ resources/secrets/secret.py
@@ -0,0 +1,1 @@
+the_secret_sauce = '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'
