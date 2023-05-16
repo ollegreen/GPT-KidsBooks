3dfa4b66bbc3bded65c7e4f679d0f3c69c2364ca
ollegreen <olleg.green@gmail.com>
2023-05-16 22:12:36 +0100

    updates

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import requests
 import streamlit as st
 
-st.write('Hello world!')
+st.write('Hello unknown storyteller!')
+
+api_key = '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'
+
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+headers = {"Authorization": f"{api_key}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+prompt = st.text_input('What story would you like?')
+
+
+if prompt:
+    output = query({
+          "inputs": f"{prompt}",
+          })
+    st.write(output)
