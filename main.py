95f66062636ab180d7ab3feb2e77d23e4b09401b
ollegreen <olleg.green@gmail.com>
2023-05-16 22:24:59 +0100

    updates to output

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+import os
 import requests
 import streamlit as st
 
+from resources.api.api_key import the_api_key
+
 st.write('Hello unknown storyteller!')
 
-api_key = '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'
-
 API_URL = "https://api-inference.huggingface.co/models/gpt2"
-headers = {"Authorization": f"{api_key}"}
+headers = {"Authorization": f"{the_api_key}"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
